bf89d39cbb7888c125e0ab93a89635e8c5ccb325
berihugebre <danigebre039@gmail.com>
2020-03-18 19:41:05 +0100

    Updates

---
+++ precache-manifest.68ea6bbf858ef676cd9743dbc839131e.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9cab0c41c12450d4d8d6121b77962d",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "84778ed6c9fac40dc4c7",
+    "url": "/Portfolio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "49af51d881712dcd7629",
+    "url": "/Portfolio/static/css/main.cb6bd365.chunk.css"
+  },
+  {
+    "revision": "84778ed6c9fac40dc4c7",
+    "url": "/Portfolio/static/js/2.3cd144e9.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/Portfolio/static/js/2.3cd144e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49af51d881712dcd7629",
+    "url": "/Portfolio/static/js/main.73749cf7.chunk.js"
+  },
+  {
+    "revision": "fefd097d40dd51df3811",
+    "url": "/Portfolio/static/js/runtime-main.1bf34bb2.js"
+  },
+  {
+    "revision": "ce03c8877ed169dba19042f45c7909b5",
+    "url": "/Portfolio/static/media/CV-Berihu.ce03c887.pdf"
+  },
+  {
+    "revision": "e2f2833e2d545778b6e63caa0cc3da1b",
+    "url": "/Portfolio/static/media/bg-3.e2f2833e.jpg"
+  },
+  {
+    "revision": "2b1619ae1adb11d89ab6b9f9f1bf6c51",
+    "url": "/Portfolio/static/media/bivak.2b1619ae.png"
+  },
+  {
+    "revision": "c715b146f1f1205cd59c384e4e562213",
+    "url": "/Portfolio/static/media/cv.c715b146.png"
+  },
+  {
+    "revision": "c770c24efb4295d23788e640bfcf9f6f",
+    "url": "/Portfolio/static/media/logo.c770c24e.png"
+  },
+  {
+    "revision": "5c4a55f6bc274ca00ec5dfd2f0ce395a",
+    "url": "/Portfolio/static/media/profilepic.5c4a55f6.jpeg"
+  },
+  {
+    "revision": "2f7314f27f7a17cac3885877ab9ed73d",
+    "url": "/Portfolio/static/media/vr.2f7314f2.png"
+  }
+]);
