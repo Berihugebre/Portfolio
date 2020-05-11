dbc115cb6b504c816d9e4140908b61d5ce302615
berihugebre <danigebre039@gmail.com>
2020-05-11 23:26:10 +0200

    ADD(testimonials)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,11 @@ const About = ()=>{
                      <h1 className="display-4 animated wow slideInLeft">About Me</h1>
                 </div>
                 <hr />
-                <div className="col-xs-12 col-sm-12 col-md-4 padding animated wow zoomIn">
-                    <img src={person.about.profilePiPicture} width="100%" alt="profile" className="rounded-circle"/>
+                <div className="col-xs-12 col-sm-12 col-md-4 padding">
+                    <figure class="figure">
+                    <img src={person.about.profilePiPicture} width="100%" alt="profile" className="rounded-circle"/>            
+                        <figcaption className="figure-caption text-center p-3">{person.about.name}</figcaption>
+                    </figure>
                 </div>
                 <div className="col-xs-12 col-sm-12 col-md-8">
                     <ul>
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,9 +6,7 @@ const Skills = ()=>{
       <div className="container-fluid padding bg-info text-white" id="skills">
         <div className="row headers text-center padding">
             <div className="col-12">
-           
                 <h1 className="display-4">Skills</h1>
-                <div className="heading-underline"></div>
             </div>
             <hr />
         </div>
@@ -27,6 +25,34 @@ const Skills = ()=>{
                 )
               })}
         </div>
+        <hr className="my-4"/>
+          <div className="row jumbotron bg-light text-dark">
+              <div className="col-12 text-center">
+                  <h3 className="heading">Testimonial</h3>
+                  <div className="heading-underline"></div>
+              </div>
+                <div className="row d-flex justify-content-center">  
+                  {person.testimonials.map((testimonial,index)=>(
+                    <div key={index} className="col-md-6 testimonial">
+                        <div className="row">
+                          <div className="col-xs-12 col-sm-12 col-md-4">
+                              <img src={testimonial.picture} alt={testimonial.name} />
+                          </div>
+                          <div className="col-xs-12 col-sm-12 col-md-8">
+                              <blockquote>
+                                <i className="fas fa-quote-left" />
+                                {testimonial.message}
+                                <i className="fas fa-quote-right" />
+                                <hr className="testimonial-hr" />
+                                <cite>&#8212;{testimonial.name}</cite> <br/>
+                                <cite>{testimonial.job}</cite>
+                              </blockquote>
+                          </div>
+                        </div>
+                    </div>
+                  ))}
+                </div>
+          </div>
         <hr className="my-4"/>
     </div>
     )
